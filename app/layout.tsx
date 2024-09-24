5a30d52baa0f8dcb54bb9b94fab4fe65c999e340
Jorshua Hernandez <jorshua321@gmail.com>
2024-09-24 16:26:50 -0500

    v2 landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,31 +53,70 @@ export default function RootLayout({
       >
         {!showContent && (
           <div
-            className="bg-slate-700"
-            style={{ height: "100vh", width: "100vw" }}
+            className="relative bg-cover bg-center"
+            style={{
+              height: "100vh",
+              width: "100vw",
+              backgroundImage: "url('/imagen3.jpg')",
+            }}
           >
-            <div className="w-full h-full flex flex-col items-center justify-center relative">
-              <div
-                id="logo"
-                className="h-40 w-full border-0 border-red-500 flex justify-center items-end p-5"
-              >
-                <span className="relative text-white scroll-m-20 text-4xl font-extrabold tracking-tight">
+            <div className="w-full h-full flex flex-col pl-8 md:pl-24 space-y-6 md:pt-52 justify-center md:justify-normal">
+              {/* Sección del Logo */}
+              <div id="logo" className="h-auto w-full flex items-start">
+                <span className="relative text-slate-900 scroll-m-20 text-3xl font-extrabold tracking-tight">
                   <TbColorFilter
                     size={40}
                     style={{ position: "absolute", top: -30, right: -25 }}
                   />
-                  Energy<span className="text-green-600">Metrics</span>
+                  Energy<span className="text-white">Metrics</span>
                 </span>
+                {/* <span className="text-white text-5xl font-extrabold tracking-tight">
+                  Sun<span className="text-blue-600">Volt</span>
+                </span> */}
               </div>
 
-              <button
-                onClick={handleClick}
-                className="transition ease-in-out delay-15 hover:-translate-y-1 rounded-md flex items-center py-3 px-6 bg-green-600 shadow-xl text-white-700 w-max hover:bg-slate-800 hover:text-slate-300"
+              {/* Título Principal */}
+              <h1 className="text-white font-bold tracking-tighter text-5xl lg:text-7xl">
+                Empowering Change <br />{" "}
+                <span className="text-slate-900 font-extrabold">
+                  One Metric
+                </span>{" "}
+                at Time
+              </h1>
+
+              {/* Descripción */}
+              <p
+                className=" text-white text-lg md:text-2xl font-light tracking-tighter w-2/3 md:p-3 rounded-2xl"
+                style={{ margin: 0 }}
               >
-                <span className="text-lg tracking-tighter">Ingresar</span>
+                Shaping Tomorrow's Energy Landscape, TODAY
+              </p>
+
+              {/* Botón de Acción */}
+              <button
+                style={{ marginTop: 40 }}
+                onClick={handleClick}
+                className="transition ease-in-out delay-15 hover:-translate-y-1 rounded-md flex items-center py-3 px-6 w-max bg-slate-700 shadow-xl text-white hover:bg-white hover:text-green-600"
+              >
+                <span className="text-lg tracking-tighter">Start</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="ml-2 h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
               </button>
 
-              <span className="absolute bottom-4 right-4 flex items-end text-slate-900 tracking-tighter text-lg font-bold">
+              {/* Créditos */}
+              <span className="absolute bottom-4 right-4 text-white text-sm font-light">
                 Tesis realizada por: Guillermo Orozco
               </span>
             </div>
